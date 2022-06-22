e13d220fc77ae68bf6c2de75f906169700fc8327
jon-personal <jonatasoak@gmail.com>
2022-06-21 23:18:46 -0400

    adding video

--- app.py
+++ app.py
@@ -59,130 +59,130 @@ with col9:
 
 if a:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>APPLE</b></p>', unsafe_allow_html=True)
-    st.image('images/apple.jpeg')
+    st.video('videos/apple.mp4')
     st.balloons()
 
 if b:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>BIRD</b></p>', unsafe_allow_html=True)
-    st.image('images/bird.jpg')
+    st.video('videos/bird.mp4')
     st.balloons()
 
 if c:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>COW</b></p>', unsafe_allow_html=True)
-    st.image('images/cow.jpg')
+    st.video('videos/cow.mp4')
     st.balloons()
 
 if d:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>DOG</b></p>', unsafe_allow_html=True)
-    st.image('images/dog.jpeg')
+    st.video('videos/dog.mp4')
     st.balloons()
 
 if e:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>ELEPHANT</b></p>', unsafe_allow_html=True)
-    st.image('images/elephant.jpg')
+    st.video('videos/elephant.mp4')
     st.balloons()
 
 if f:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>FROG</b></p>', unsafe_allow_html=True)
-    st.image('images/frog.jpeg')
+    st.video('videos/frog.mp4')
     st.balloons()
 
 if g:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>GIRAFFE</b></p>', unsafe_allow_html=True)
-    st.image('images/giraffe.jpg')
+    st.video('videos/giraffe.mp4')
     st.balloons()
 
 if h:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>HORSE</b></p>', unsafe_allow_html=True)
-    st.image('images/horse.jpeg')
+    st.video('videos/horse.mp4')
     st.balloons()
 
 if i:
-    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>IMPALA</b></p>', unsafe_allow_html=True)
-    st.image('images/impala.jpg')
+    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>ICE CREAM</b></p>', unsafe_allow_html=True)
+    st.video('videos/ice_cream.mp4')
     st.balloons()
 
 if j:
-    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>JAGUAR</b></p>', unsafe_allow_html=True)
-    st.image('images/jaguar.jpeg')
+    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>JELLYFISH</b></p>', unsafe_allow_html=True)
+    st.video('videos/jellyfish.mp4')
     st.balloons()
 
 if k:
-    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>KANGAROO</b></p>', unsafe_allow_html=True)
-    st.image('images/kangaroo.jpg')
+    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>KEY</b></p>', unsafe_allow_html=True)
+    st.video('videos/key.mp4')
     st.balloons()
 
 if l:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>LION</b></p>', unsafe_allow_html=True)
-    st.image('images/lion.jpg')
+    st.video('videos/lion.mp4')
     st.balloons()
 
 if m:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>MONKEY</b></p>', unsafe_allow_html=True)
-    st.image('images/monkey.jpg')
+    st.video('videos/monkey.mp4')
     st.balloons()
 
 if n:
-    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>NEWT</b></p>', unsafe_allow_html=True)
-    st.image('images/newt.jpg')
+    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>NO</b></p>', unsafe_allow_html=True)
+    st.video('videos/no.mp4')
     st.balloons()
 
 if o:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>OWL</b></p>', unsafe_allow_html=True)
-    st.image('images/owl.jpg')
+    st.video('videos/owl.mp4')
     st.balloons()
 
 if p:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>PENGUIN</b></p>', unsafe_allow_html=True)
-    st.image('images/penguin.jpg')
+    st.video('videos/penguin.mp4')
     st.balloons()
 
 if q:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>QUIET</b></p>', unsafe_allow_html=True)
-    st.image('images/quiet.jpeg')
+    st.video('videos/quiet.mp4')
     st.balloons()
 
 if r:
-    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>RHINO</b></p>', unsafe_allow_html=True)
-    st.image('images/rhino.jpg')
+    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>RAINBOW</b></p>', unsafe_allow_html=True)
+    st.video('videos/rainbow.mp4')
     st.balloons()
 
 if s:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>SQUIRREL</b></p>', unsafe_allow_html=True)
-    st.image('images/squirrel.jpeg')
+    st.video('videos/squirrel.mp4')
     st.balloons()
 
 if t:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>TURTLE</b></p>', unsafe_allow_html=True)
-    st.image('images/turtle.jpg')
+    st.video('videos/turtle.mp4')
     st.balloons()
 
 if u:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>UMBRELLA</b></p>', unsafe_allow_html=True)
-    st.image('images/umbrella.jpeg')
+    st.video('videos/umbrella.mp4')
     st.balloons()
 
 if v:
-    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>VACUUM</b></p>', unsafe_allow_html=True)
-    st.image('images/vacuum.jpg')
+    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>VOLCANO</b></p>', unsafe_allow_html=True)
+    st.video('videos/volcano.mp4')
     st.balloons()
 
 if w:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>WATERMELON</b></p>', unsafe_allow_html=True)
-    st.image('images/watermelon.jpg')
+    st.video('videos/watermelon.mp4')
     st.balloons()
 
 if x:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>XYLOPHONE</b></p>', unsafe_allow_html=True)
-    st.image('images/xylophone.jpeg')
+    st.video('videos/xylophone.mp4')
     st.balloons()
 
 if y:
-    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>YOGURT</b></p>', unsafe_allow_html=True)
-    st.image('images/yogurt.jpg')
+    st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>YES</b></p>', unsafe_allow_html=True)
+    st.video('videos/yes.mp4')
     st.balloons()
 
 if z:
     st.markdown('<p style=" color: #e65eaf; font-size: 50px; text-align: center;"><b>ZEBRA</b></p>', unsafe_allow_html=True)
-    st.image('images/zebra.jpg')
+    st.video('videos/zebra.mp4')
     st.balloons()
